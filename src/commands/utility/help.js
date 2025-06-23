bd6788a0eca04686eb2d1d4ba1fea3e328554e51
FYMajid <fathiyakanmajid81@gmail.com>
2025-06-23 19:21:31 +0700

    add feature help final

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  MessageFlags,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -39,6 +43,9 @@ module.exports = {
       });
     }
 
-    await interaction.reply({ embeds: [helpEmbed], ephemeral: true });
+    await interaction.reply({
+      embeds: [helpEmbed],
+      flags: [MessageFlags.Ephemeral],
+    });
   },
 };
